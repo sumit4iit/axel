b5556c55059fc37c1dc958339513c4172e8c9a88
sumit agrawal <sumit4iit@gmail.com>
2015-10-23 09:28:00 +0530

    Global error handler

---
+++ errors.h
@@ -0,0 +1,36 @@
+ /********************************************************************\
+  * Axel -- A lighter download accelerator for Linux and other Unices. *
+  *                                                                    *
+  * Copyright 2001 Wilmer van der Gaast                                *
+  * Author: Sumit Agrawal (sumit4iit@gmail.com)						   *
+  \********************************************************************/
+
+/* Configuration handling include file					*/
+
+/*
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License with
+  the Debian GNU/Linux distribution in file /usr/doc/copyright/GPL;
+  if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+  Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#define SC_FAILED(error) 	\
+	(error == 0) false : true;
+
+enum STATUSCODE
+{
+	AXEL_ERROR_SUCCESS = 0,
+	AXEL_ERROR_FAILED,
+	AXEL_ERROR_INVALID_ARGUMENT,
+	AXEL_ERROR_NULL_POINTER_DEREF
+};
